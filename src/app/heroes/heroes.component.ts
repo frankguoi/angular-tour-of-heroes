e08fd29d5420dbaabe3f86ea9960175d64bb2dfc
Frank Guoi <frank.guoi@hotmail.fr>
2021-02-15 16:11:44 +0100

    Inject the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from "../hero.service";
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes: Hero[];
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
   }
